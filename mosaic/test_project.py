cbda5413e6455d6bc7e3e10d7242afe2050b1a8a
John <salvadorjmassa@gmail.com>
2022-07-17 12:22:40 -0400

    Added test_get_portrait_invalid() to test get_portrait for not finding portraits. Removed test_valid() as it was an unnecessary function.

--- mosaic/test_project.py
+++ mosaic/test_project.py
@@ -15,27 +15,29 @@ import pytest
 import mock
 
 from project import get_input
-from project import valid_url
+from project import valid_url_tiles
 from project import download_image
 from project import verify_CLA
+from project import get_portrait
 
 # monkeypatch replaces the input() function, temporarily,
 # with a string from the input list
 def test_get_input_valid(monkeypatch):
-    names = ["Claude Monet", "Vincent Van Gogh  ", "    REMBRANDT", "leonardo DA VincI"]
+    names = iter(["Claude Monet", "Vincent Van Gogh  ", " ", "    REMBRANDT", "      ", "\n", "leonardo DA VincI"])
     results = ["claude monet", "vincent van gogh", "rembrandt", "leonardo da vinci"]
-    for (name, result) in zip(names, results):
-        monkeypatch.setattr('builtins.input', lambda: name)
+    for result in results:
+        monkeypatch.setattr('builtins.input', lambda: next(names))
         assert get_input() == result
-
-def test_valid_url():
-    with open("test_files/url_test_list_validity.txt") as infile:
-        reader = infile.readlines()
-        for line in reader:
-            assert valid_url(line.strip()) == True
 
 def test_verify_CLA(monkeypatch):
     pathname = os.path.join(os.getcwd(), "test_files/test_images")
     for file in os.listdir(pathname):
         monkeypatch.setattr('sys.argv', [os.path.join(pathname, file), pathname])
         assert verify_CLA() == None
+
+def test_get_portrait_invalid(capsys):
+    with open("test_files/get_portrait_test_invalid.txt") as infile:
+        reader = infile.readlines()
+        for line in reader:
+            with pytest.raises(SystemExit):
+                assert get_portrait(line.strip()) == "No portraits found."
